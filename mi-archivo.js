194d2345f44e2ab7af89cbcdd89b2f7446f406bd
Ianbreness <brenes.ian.cr7@gmail.com>
2025-01-20 20:26:07 -0600

    Update mi-archivo.js
    Editar mensaje y agregar consola en mi-archivo.js

--- mi-archivo.js
+++ mi-archivo.js
@@ -1,1 +1,6 @@
+// Este es el archivo inicial con un mensaje de alerta
 alert("hola mundo");
+
+// Este es el archivo inicial con un mensaje de alerta
+alert("hola mundo_editado");
+console.log("cambios en el archivo");
